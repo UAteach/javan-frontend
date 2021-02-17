b29949259bdae1765d603af5db9b19dedfb15f54
RyanRau <ryanzrau@gmail.com>
2021-02-16 18:57:12 -0600

    unauth inventory view fix

--- src/app/materials-view-inventory-page/materials-view-inventory-page.component.ts
+++ src/app/materials-view-inventory-page/materials-view-inventory-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ItemDTO } from '../models';
 import { ItemsService } from '../services';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-materials-view-inventory-page',
@@ -11,18 +12,19 @@ export class MaterialsViewInventoryPageComponent implements OnInit {
 
   items: ItemDTO[];
   isLoading: boolean = true;
-
-  
+  isAuthenticated = false;
 
   constructor(
     private _itemsService: ItemsService,
+    private _authService: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.isAuthenticated = this._authService.isAuthenticated();
+
     this._itemsService.getAll().subscribe(items => {
       this.items = items;
       this.isLoading = false;
     })
   }
-
 }
